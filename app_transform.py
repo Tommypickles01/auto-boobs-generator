1d69530d571c66ffb47f7c63c093da289ca083b3
Tommypickles01 <vuongthomas1@yahoo.com>
2025-07-30 12:54:00 -0500

    Update app_transform.py

--- app_transform.py
+++ app_transform.py
@@ -110,4 +110,6 @@ def transform():
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
